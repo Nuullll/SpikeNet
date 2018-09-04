cb0a3bcbadae35d9b6d0447ff5439a73e3ba69ff
Nuullll <vfirst218@gmail.com>
2018-09-04 21:36:13 +0800

    Implement Connection>

---
+++ snet/network/connection.py
@@ -0,0 +1,22 @@
+# @Author: Yilong Guo
+# @Date:   04-Sep-2018
+# @Email:  vfirst218@gmail.com
+# @Filename: connection.py
+# @Last modified by:   Yilong Guo
+# @Last modified time: 04-Sep-2018
+
+
+class Connection:
+    """
+    <Connection> describes the weight matrix between two <Layer>s of <Neuron>s.
+    """
+    def __init__(self, pre_layer, post_layer, weight):
+        """
+        Initialize a <Connection> which connects from `pre_layer` to `post_layer`.
+        :param pre_layer:       <Layer>
+        :param post_layer:      <Layer>
+        :param weight:          <np.ndarray>        Matches the sizes of `pre_layer` and `post_layer`.
+        """
+        self.pre_layer = pre_layer
+        self.post_layer = post_layer
+        self.weight = weight
