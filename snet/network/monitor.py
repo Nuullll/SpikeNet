53998e410dcb46d4c87736e5f9adfdea97c19670
Nuullll <vfirst218@gmail.com>
2018-09-04 22:16:34 +0800

    Implement Monitor

---
+++ snet/network/monitor.py
@@ -0,0 +1,21 @@
+# @Author: Yilong Guo
+# @Date:   04-Sep-2018
+# @Email:  vfirst218@gmail.com
+# @Filename: monitor.py
+# @Last modified by:   Yilong Guo
+# @Last modified time: 04-Sep-2018
+
+
+class Monitor:
+    """
+    Record state variables in specific <Layer>.
+    """
+    def __init__(self, target, state_vars):
+        """
+        Creates a <Monitor> recording `state_vars` in `target` layer.
+        :param target:          <Layer>
+        :param state_vars:      [str]
+        """
+        self.target = target
+        self.state_vars = state_vars
+        self.record = {state: [] for state in state_vars}
