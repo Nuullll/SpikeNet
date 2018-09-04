57ca25c14db78b8c29b119323ea37595bdacffc4
Nuullll <vfirst218@gmail.com>
2018-09-04 22:17:10 +0800

    Implement NetworkBuilder

--- snet/network/__init__.py
+++ snet/network/__init__.py
@@ -7,29 +7,71 @@
 
 
 from .layer import *
+from .connection import *
+from .monitor import *
 
 
-class NetworkConfig:
+class NetworkBuilder:
     """
-    Configuration for <Network>.
+    Network builder, with given <Layer>s, <np.ndarray> weights, <Monitor>s.
     """
-    def __init__(self):
-        self.dt = 1.0
-        self.layers = {}
-        self.connections = {}
-        self.monitors = {}
+    def __init__(self, layers, weights, monitors, dt=1.0):
+        """
+        Initialize builder.
+        :param layers:      dict        {name: <Layer>}
+        :param weights:     dict        {(pre_layer_name, post_layer_name): weights}
+        :param monitors:    dict        {name: [state_vars]}
+        :param dt:          float       Simulation time step.
+        """
+        self.network = Network(dt=dt)
+
+        # initialize layers
+        self.network.layers = layers
+
+        # initialize connections
+        self._init_connections(weights)
+
+        # initialize monitors
+        self._init_monitors(monitors)
+
+    def _init_connections(self, weights):
+        """
+        Instantiate <Connection> objects.
+        :param weights:     dict        {(pre_layer_name, post_layer_name): weights}
+        """
+        for key, weight in weights.items():
+            pre_layer_name, post_layer_name = key
+            pre_layer = self.network.layers[pre_layer_name]
+            post_layer = self.network.layers[post_layer_name]
+            conn = Connection(pre_layer=pre_layer, post_layer=post_layer, weight=weight)
+            self.network.connections[(pre_layer_name, post_layer_name)] = conn
+
+    def _init_monitors(self, monitors):
+        """
+        Instantiate <Monitor> objects.
+        :param monitors:    dict        {name: [state_vars]}
+        """
+        for name, state_vars in monitors.items():
+            mon = Monitor(target=self.network.layers[name], state_vars=state_vars)
+            self.network.monitors[name] = mon
+
+    def build(self):
+        """
+        :return:    <Network>
+        """
+        return self.network
 
 
 class Network:
     """
     Responsible for interaction simulation among neurons and synapses.
     """
-    def __init__(self, config):
+    def __init__(self, dt=1.0):
         """
         Initialize <Network> instance, with a configuration.
-        :param config:      <NetworkConfig>         Configuration.
         """
-        self.config = config
-        self.layers = config.layers
-        self.connections = config.connections
-        self.monitors = config.monitors
+        self.layers = {}            # {name: <Layer>}
+        self.connections = {}       # {(pre_layer_name, post_layer_name): <Connection>}
+        self.monitors = {}          # {layer_name: <Monitor>}
+
+        self.dt = dt
